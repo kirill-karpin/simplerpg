2dea40bd4e0fdc239e21560f28b1986bcd4624d3
PhpAnon <snowfrost@mail.ru>
2013-09-10 13:12:06 +0600

    ffdf

---
+++ ttete.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kirill
+ * Date: 10.09.13
+ * Time: 12:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "test";
